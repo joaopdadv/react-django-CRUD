2ada44390c4da591c3266f420b14f2eac430e282
João Pedro <joaopdadv@gmail.com>
2025-12-13 13:53:27 -0300

    [style]: correcoes na responsividade da tabela de funcionários

--- frontend/src/components/funcionarios/FuncionariosTable.tsx
+++ frontend/src/components/funcionarios/FuncionariosTable.tsx
@@ -23,6 +23,9 @@ import type { Page } from '@/types/pagination';
 import { useFuncionarios } from '@/context/funcionariosContext';
 import { toast } from 'sonner';
 import FuncionariosPagination from './FuncionariosPagination';
+import { ScrollArea } from '../ui/scroll-area';
+import { truncateText } from '@/utils/truncateText';
+import { TooltipContent } from '@radix-ui/react-tooltip';
 
 interface FuncionariosTableProps { }
 
@@ -45,16 +48,15 @@ const FuncionariosTable: React.FC<FuncionariosTableProps> = () => {
 
     return (
         <div className='w-[90%] mx-auto h-full flex flex-col flex-1 py-4'>
-            <div className='flex-1 overflow-auto'>
+            <ScrollArea className='overflow-auto grow h-5 overflow-x-hidden px-2' type='always' >
                 <Table>
                     <TableHeader>
                         <TableRow>
                             <TableHead className="w-[120px]">ID</TableHead>
                             <TableHead>Nome</TableHead>
                             <TableHead>CPF</TableHead>
-                            <TableHead>Email</TableHead>
                             <TableHead>Cargo</TableHead>
-                            <TableHead className="text-right">Status</TableHead>
+                            <TableHead>Status</TableHead>
                         </TableRow>
                     </TableHeader>
                     <TableBody>
@@ -62,16 +64,15 @@ const FuncionariosTable: React.FC<FuncionariosTableProps> = () => {
                         {funcionarios?.results?.map((f) => (
                             <TableRow key={String(f.id)}>
                                 <TableCell className="font-medium">{f.id ?? '-'}</TableCell>
-                                <TableCell>{f.nome ?? '-'}</TableCell>
-                                <TableCell>{f.sobrenome ?? '-'}</TableCell>
+                                <TableCell>{f.nome ? truncateText(f.nome + " " + f.sobrenome, 30) : '-'}</TableCell>
                                 <TableCell>{f.cpf ? f.cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4') : '-'}</TableCell>
-                                <TableCell>{f.cargo ?? '-'}</TableCell>
+                                <TableCell><p>{f.cargo ? truncateText(f.cargo, 20) : '-'}</p></TableCell>
                                 <TableCell>{f.ativo ? 'Ativo' : 'Inativo'}</TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
                 </Table>
-            </div>
+            </ScrollArea>
             <div className='py-4'>
                 <FuncionariosPagination currentPage={page} lastPage={Math.ceil(funcionarios.count / pageSize)} />
             </div>
